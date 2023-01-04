45218afa7c0fdb97a6e38d4ecfa6340bd0145b44
Guy Gabel <guy.gabel0@gmail.com>
2023-01-04 21:02:37 +0200

    Third commit and push

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,10 +1,16 @@
 import org.testng.annotations.Test;
 
 public class MyTest {
-    @Test
+    @Test (priority = 1)
     public void firstTest() {
         System.out.println("wow, a test");
         System.out.println("first commit");
         System.out.println("another try");
+
+
+        }
+    @Test (priority = 2)
+    public void secondTest() {
+        System.out.println("new test was added");
     }
 }
