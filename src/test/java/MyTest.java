d289359e6eb99827a03c5fb8cb7b15a3077287d6
Guy Gabel <guy.gabel0@gmail.com>
2023-01-04 20:56:04 +0200

    Second commit and push

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -5,5 +5,6 @@ public class MyTest {
     public void firstTest() {
         System.out.println("wow, a test");
         System.out.println("first commit");
+        System.out.println("another try");
     }
 }
