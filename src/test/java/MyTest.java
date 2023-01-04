597d351adac8889c6d63d78737e4f10b93b9cd68
Guy Gabel <guy.gabel0@gmail.com>
2023-01-04 20:53:57 +0200

    first commit and push

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class MyTest {
+    @Test
+    public void firstTest() {
+        System.out.println("wow, a test");
+        System.out.println("first commit");
+    }
+}
